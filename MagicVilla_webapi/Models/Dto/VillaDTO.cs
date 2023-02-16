76bae4114a76c7c82a68cae66052afd507982ea5
ravi <ravi30445@gmail.com>
2023-02-16 17:38:12 +0530

    added controller and model for magicVillaAPI

---
+++ MagicVilla_webapi/Models/Dto/VillaDTO.cs
@@ -0,0 +1,7 @@
+namespace MagicVilla_webapi.Models.Dto
+{
+    public class VillaDTO{
+        public int id{get;set;}
+        public string name{get;set;}
+    }
+}
---
+++ MagicVilla_webapi/Models/Villa.cs
@@ -0,0 +1,6 @@
+namespace MagicVilla_webapi.Models{
+    public class Villa{
+        public int id{get;set;}
+        public string name{get;set;}
+    }
+}
